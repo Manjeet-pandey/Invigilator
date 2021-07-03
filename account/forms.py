4a11917e31c7d39b14e9005f134b052301e440a2
Beeps <beeps@kali.beeps>
2021-07-03 17:11:35 +0545

    commited

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from account.models import Teacher
-#from django import forms
+# from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -11,18 +11,18 @@ class RegisterForm(ModelForm):
     
     def TeacherForm():
         class Meta:
-            model = Teacher
+            models = Teacher
             fields = ["first_Name", "middle_Name", "last_Name", "dob", "phone_Num", "gender",
                     "qualification", "photo", "email_Id"]
     
     def StaffForm():
         class Meta:
-            model = Teacher
+            models = Teacher
             fields = ["first_Name", "middle_Name", "last_Name", "dob", "phone_Num", "gender",
                       "qualification", "photo", "email_Id"]
 
     def StudentForm():
         class Meta:
-            model = Teacher
+            models = Teacher
             fields = ["first_Name", "middle_Name", "last_Name", "dob", "phone_Num", "gender",
                       "qualification", "photo", "email_Id"]
